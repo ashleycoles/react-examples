bc81b1a1ca460bb0544aafadd70c7dbc7aaee798
Ashley Coles <ashley@acoles.co.uk>
2021-06-10 08:15:45 +0100

    Create usestate-example.js

---
+++ usestate-example.js
@@ -0,0 +1,14 @@
+function App() {
+  // useState allows us to create local state in functional components
+  // [stateName, functionToChangeState] = useState(initial value)
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+        <p>You clicked {count} times</p>
+        <button onClick={()=>setCount(count + 1)}>
+            Click me
+        </button>
+    </div>
+  );
+}
